5b279ed334f01284f516a52fc8133681c290e430
Polnfuel <polnfuel@gmail.com>
2025-01-17 15:03:04 +0200

    Can createfield

--- src/App.js
+++ src/App.js
@@ -2,17 +2,6 @@ import './App.css';
 import Grid from './Grid';
 import React, {useState} from 'react';
 
-let Field, UserField, DField, Width, Height, MinesCount, GameStatus;
-let curwidth = 10, curheight = 12, curmines = 10;
-
-function arrayfill(field, value) {
-    for (let i = 0; i < field.length; i++){
-        for (let j = 0; j < field[0].length; j++){
-            field[i][j] = value;
-        }
-    }
-    return field;
-}
 function GetNeighboors(i, j, field) {
     let count = 0;
     let mas;
@@ -68,48 +57,65 @@ function GetNeighboors(i, j, field) {
     });
     return count;
 }
-function newfield() {
-    Width = curwidth;
-    Height = curheight;
-    MinesCount = curmines;
-    GameStatus = "Inited";
-    Field = Array.from({length: Width}, () => Array.from({length: Height}, () => null) );
+
+function App() {
+  let [Width, setWidth] = useState(10);
+  let [Height, setHeight] = useState(12);
+  let [MinesCount, setMinesCount] = useState(11);
+  let [curwidth, setcurwidth] = useState(10);
+  let [curheight, setcurheight] = useState(12);
+  let [curmines, setcurmines] = useState(11);
+  let [Field, setField] = useState(Array.from({length: Height}, () => Array.from({length: Width}, () => null)));
+  let [UserField, setUserField] = useState(Array.from({length: Height}, () => Array.from({length: Width}, () => null)));
+  let [DField, setDField] = useState(Array.from({length: Width * Height}, () => null));
+  let [GameStatus, setGameStatus] = useState(null);
+
+  function createfield(){
+    let width = curwidth;
+    let height = curheight;
+    let minescount = curmines;
+    let field = Array.from({length: width}, () => Array.from({length: height}, () => null) );
     let mines = [];
     let newmine;
-    if (MinesCount < Width * Height){
-        for (let i = 0; i < MinesCount; i++){
+    if (minescount < width * height){
+        for (let i = 0; i < minescount; i++){
             do {
-                newmine = Math.floor(Math.random() * (Width * Height));
+                newmine = Math.floor(Math.random() * (width * height));
                 mines[i] = newmine;
             } while (mines.indexOf(newmine) !=  mines.lastIndexOf(newmine));
         }
         mines.forEach(mine => {
-            Field[Math.floor(mine / Height)][mine % Height] = "M";
+            field[Math.floor(mine / height)][mine % height] = "M";
         });
-        for (let i = 0; i < Width; i++){
-            for (let j = 0; j < Height; j++){
-                if (Field[i][j] != "M"){
-                    Field[i][j] = String(GetNeighboors(i, j, Field));
+        for (let i = 0; i < width; i++){
+            for (let j = 0; j < height; j++){
+                if (field[i][j] != "M"){
+                    field[i][j] = String(GetNeighboors(i, j, field));
                 }
             }
         }
     }
     else{
-        Field = new Array(Width).fill(null).map(() => new Array(Height).fill("M"));
+        field = new Array(width).fill(null).map(() => new Array(height).fill("M"));
     }
-    UserField = new Array(Width).fill(null).map(() => new Array(Height).fill("C"));
+    setField(field);
+    let userField = new Array(width).fill(null).map(() => new Array(height).fill("C"));
+    setUserField(userField);
+    setWidth(curwidth);
+    setHeight(curheight);
+    setMinesCount(curmines);
+    setGameStatus("Inited");
     create1dfield(Field);
-}
-function create1dfield(field) {
-  DField = new Array(field.length * field[0].length);
-    for (let i = 0; i < field.length; i++){
-        for (let j = 0; j < field[0].length; j++){
-            DField[i * field[0].length + j] = field[i][j];
-        }
-    }
-}
-newfield();
-function App() {
+  }
+  function create1dfield(field) {
+    let dField = new Array(field.length * field[0].length);
+      for (let i = 0; i < field.length; i++){
+          for (let j = 0; j < field[0].length; j++){
+              dField[i * field[0].length + j] = field[i][j];
+          }
+      }
+    setDField(dField);
+  }
   return (
     <div className="app">
       <Grid width={Width} height={Height} field={UserField}/>
@@ -117,16 +123,16 @@ function App() {
       <div className='forma container mt-5'>
         <form className='row g-3'>
           <div className='col-auto'>
-            <input value={curwidth} type='number' className='form-control' placeholder='Высота' />
+            <input value={curwidth} onChange={(e) => setcurwidth(Number(e.target.value))} type='number' className='form-control' placeholder='Высота' />
           </div>
           <div className='col-auto'>
-            <input value={curheight} type='number' className='form-control' placeholder='Ширина' />
+            <input value={curheight} onChange={(e) => setcurheight(Number(e.target.value))} type='number' className='form-control' placeholder='Ширина' />
           </div>
           <div className='col-auto'>
-            <input value={curmines} type='number' className='form-control' placeholder='Кол-во мин' />
+            <input value={curmines} onChange={(e) => setcurmines(Number(e.target.value))} type='number' className='form-control' placeholder='Кол-во мин' />
           </div>
           <div className='col-auto'>
-            <button type='button' onClick={newfield}>Создать</button>
+            <button type='button' onClick={createfield}>Создать</button>
           </div>
         </form>
       </div>
