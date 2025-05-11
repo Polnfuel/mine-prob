991452de14117e731fb4fa5d1bf1881a903b46c5
Polnfuel <polnfuel@gmail.com>
2025-05-11 14:06:18 +0300

    Fix new base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/minesweeper/',
+  base: '/mine-prob/',
   plugins: [react()],
   server: {
     headers: {
